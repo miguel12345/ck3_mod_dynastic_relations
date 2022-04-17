4af6b95d80fadd5b71053ef1477f863a650501a3
Miguel Ferreira <amigueloferreira@gmail.com>
2022-04-17 09:41:16 +0100

    lots of improvements, checkpoint before moving on to the UI

--- tools/debug_mod.bat
+++ tools/debug_mod.bat
@@ -1,6 +1,6 @@
 copy "dynamic_relations.mod" "C:\Users\amigu\Documents\Paradox Interactive\Crusader Kings III\mod\dynamic_relations.mod"
 copy "C:\Users\amigu\Documents\Paradox Interactive\Crusader Kings III\dlc_load.json" "C:\Users\amigu\Documents\Paradox Interactive\Crusader Kings III\dlc_load.json.bk"
 copy "_dlc_load.json" "C:\Users\amigu\Documents\Paradox Interactive\Crusader Kings III\dlc_load.json"
-start /wait /d "C:\Program Files (x86)\Steam\steamapps\common\Crusader Kings III\binaries" ck3.exe -debug_mode
+start /wait /d "C:\Program Files (x86)\Steam\steamapps\common\Crusader Kings III\binaries" ck3.exe -debug_mode -develop
 copy "C:\Users\amigu\Documents\Paradox Interactive\Crusader Kings III\dlc_load.json.bk" "C:\Users\amigu\Documents\Paradox Interactive\Crusader Kings III\dlc_load.json"
 DEL  "C:\Users\amigu\Documents\Paradox Interactive\Crusader Kings III\dlc_load.json.bk"
